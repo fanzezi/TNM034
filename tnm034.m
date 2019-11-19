3e076643330e99b2d4287decabe2c30ce922aa9d
Emma Algotsson <emmaalgotsson@emmas-macbook.clients.eduroam.liu.se>
2019-11-19 12:16:48 +0100

    uppdaterad

--- tnm034.m
+++ tnm034.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%% 
-function id = tnm034(im) 
+%function id = tnm034(im) 
 % 
 % im: Image of unknown face, RGB-image in uint8 format in the 
 % range [0,255] 
@@ -12,3 +12,25 @@ function id = tnm034(im)
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 % Translatera, rotera, skala, tonv?rde, kombinationer.
 % returnera 0 f?r ok?nda ansikten
+% detektera ansiktet
+% normalisera ansiktet
+
+image = imread('db1_14.jpg');
+%image = imresize(image, [0, 255]);
+image = 255*image;
+YCbCr = rgb2ycbcr(image);
+
+Y = YCbCr(:,:,1); 
+Cb = YCbCr(:,:,2);
+Cr = YCbCr(:,:,3);
+ 
+% a = uint8(zeros(536,414));
+% just_cb = cat(3,a,Cb,a);
+% just_cr = cat(3,a,a,Cr);
+
+EyeMapC = ((Cb.^2) +((255-Cr).^2) + (Cb./Cr))/3;
+%EyeMapC = ((just_cb.^2) +((255-just_cr).^2) + (just_cb./just_cr))/3;
+%EyeMapL = ...
+
+figure
+imshow(EyeMapC)
