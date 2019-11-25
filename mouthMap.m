6ee0eac7036185d77279243b2d24f97906681cbe
Emma Algotsson <emmaalgotsson@Emmas-MacBook.lan>
2019-11-25 17:55:32 +0100

    Mouth Map

---
+++ mouthMap.m
@@ -0,0 +1,22 @@
+function mouthMap = mouthMap(im)
+
+YCbCr = rgb2ycbcr(im); %rgb till ycbcr
+YCbCr = im2double(YCbCr); %change to double
+
+Cb = YCbCr(:,:,2);
+Cr = YCbCr(:,:,3);
+
+crcb = Cr./Cb; %cr is larger than cb in mouth region
+cr2 = Cr.^2;
+
+%mean of cr^2 and cr/cb to get eta
+eta = 0.95*mean(cr2(:))/mean(crcb(:));
+
+mouthMap = cr2.*((cr2-(eta.*crcb)).^2);
+
+SE = strel('disk', 10);
+mouthMap = imclose(mouthMap, SE);
+%mouthMap = imdilate(mouthMap, SE);
+
+end
+
