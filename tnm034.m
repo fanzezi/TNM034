30c68e0574d99c568f3e03a6f315b12fc61b5e8a
Fanny Andersson <fanzezi@gmail.com>
2019-11-28 10:06:21 +0100

    facemask and new functions

--- tnm034.m
+++ tnm034.m
@@ -14,14 +14,12 @@
 % returnera 0 f?r ok?nda ansikten
 % detektera ansiktet
 % normalisera ansiktet
+clc
 
-image = imread('DB1/db1_01.jpg');
+image = imread('DB1/db1_10.jpg');
 
 % Color Correlation with Gray World compensation
 image = colorCorr(image);
-
-%EyeMap, EyemapC and EyeMapL
-%Creating EyemapC & EyeMapL
 
 %Get image to YCbCr
 YCbCr = rgb2ycbcr(image);
@@ -31,27 +29,13 @@ Y = YCbCr(:,:,1);
 Cb = YCbCr(:,:,2);
 Cr = YCbCr(:,:,3);
 
-%EyeMapC
-g = 1./3;
-ccb = Cb.^2;
-ccr = (1 - Cr).^2;
-cbcr = Cb./Cr;
 
-EyeMapC = g*(ccb + ccr + cbcr);
+%Creating a EyeMap
+Eyemap = getEyeMap(Y, Cr, Cb);
 
-% EyeMapL
-%Structure Element
-SE = strel('disk',10);
-o = imdilate(Y, SE);
-p = imerode(Y, SE) + 1;
 
-EyeMapL = o./p;
+%Creating a FaceMask
+FaceMask = facemask(Cr, Cb);
 
-%imshow(EyeMapL)
-
-EyeMap = EyeMapC.*EyeMapL;
-Eyemap = imdilate(EyeMap, SE);
-
-%imshow(EyeMap) 
-%------------------------
+imshow(FaceMask)
 
