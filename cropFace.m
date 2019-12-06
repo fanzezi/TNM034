1d7edece40c2364025a8fc5ec2076580a1e2820c
Emma Algotsson <emmaalgotsson@c-10-253-236-249.clients.eduroam.liu.se>
2019-12-06 14:02:55 +0100

    eyes and mouth position now

--- cropFace.m
+++ cropFace.m
@@ -1,19 +1,33 @@
-function [faceCrop] = cropFace(im)
+function [faceCrop] = cropFace(im, eyePos1, eyePos2, mouthPos)
 
-gray = rgb2gray(im);
-binary = gray < 255;
+eyeCenter = round((eyePos1 + eyePos2)./2);
+eyedist = round(abs(eyePos1 - eyePos2));
+dist = round(abs(eyeCenter - mouthPos));
 
-[x, y] = find(binary);
-
-xmin = min(x(:));
-ymin= min(y(:));
-xmax = max(x(:));
-ymax= max(y(:));
+xmin = max((eyeCenter(1) - eyedist(1)),0);
+xmax = xmin + 2*eyedist(1);
+ymin = max((eyeCenter(2)-(3/5 * dist(2))), 0);
+ymax = ymin + 11/5 * dist(2);
 
 width = xmax-xmin;
 height = ymax-ymin;
+faceCrop = imcrop(im, [xmin ymin width height]);
 
-faceCrop = imcrop(im, [xmin, ymin, width, height]);
+
+% gray = rgb2gray(im);
+% binary = gray < 255;
+%[x, y] = find(binary);
+
+% xmin = min(x(:));
+% ymin= min(y(:));
+% xmax = max(x(:));
+% ymax= max(y(:));
+
+% width = abs(xmax-xmin);
+% height = abs(ymax-ymin);
+% 
+% faceCrop = imcrop(im, [xmin, ymin, width, height]);
+
 
 end
 
