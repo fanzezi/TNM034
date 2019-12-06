cf3df75e80672666bda6dc5f430e88a949d2d0f5
Emma Algotsson <emmaalgotsson@c-10-253-236-249.clients.eduroam.liu.se>
2019-12-06 14:39:03 +0100

    Uppdaterad med kommentarer

--- cropFace.m
+++ cropFace.m
@@ -1,33 +1,16 @@
 function [faceCrop] = cropFace(im, eyePos1, eyePos2, mouthPos)
 
-eyeCenter = round((eyePos1 + eyePos2)./2);
-eyedist = round(abs(eyePos1 - eyePos2));
-dist = round(abs(eyeCenter - mouthPos));
+eyeCenter = round((eyePos1 + eyePos2)./2); % find center between eyes
+eyedist = round(abs(eyePos1 - eyePos2)); % find distance between eyes
+dist = round(abs(eyeCenter - mouthPos)); % find distance between mouth and eyes
 
-xmin = max((eyeCenter(1) - eyedist(1)),0);
-xmax = xmin + 2*eyedist(1);
-ymin = max((eyeCenter(2)-(3/5 * dist(2))), 0);
-ymax = ymin + 11/5 * dist(2);
+xmin = max((eyeCenter(1) - eyedist(1)),0); % min X value
+ymin = max((eyeCenter(2)-(3/5 * dist(2))), 0); % min Y value, 1*dist(2) cuts of bit of mouth 
 
-width = xmax-xmin;
-height = ymax-ymin;
+width = 2*eyedist(1); %horizontal distant
+height = 2*dist(2); %vertical distant, 2* crops chin and x>2 dont
+
 faceCrop = imcrop(im, [xmin ymin width height]);
-
-
-% gray = rgb2gray(im);
-% binary = gray < 255;
-%[x, y] = find(binary);
-
-% xmin = min(x(:));
-% ymin= min(y(:));
-% xmax = max(x(:));
-% ymax= max(y(:));
-
-% width = abs(xmax-xmin);
-% height = abs(ymax-ymin);
-% 
-% faceCrop = imcrop(im, [xmin, ymin, width, height]);
-
 
 end
 
