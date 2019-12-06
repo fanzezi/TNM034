71b45d54b5ab8678958164a285a311d5c7a9f1ff
Fanny Andersson <fanzezi@gmail.com>
2019-12-06 10:53:22 +0100

    Eye Pos found

--- rotateImage.m
+++ rotateImage.m
@@ -1,24 +1,15 @@
-function [ImgRot] = rotateImage(im)
+function [ImgRot] = rotateImage(eyePos1, eyePos2, mask)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
-%Find eyes pos
-
-
-% find point2 (point with larger x)
-if A(1)>B(1)
-  P2 = A; P1 = B;
-else
-  P2 = B; P1 = A;
-end
-% clockwise OR counterclockwise
-if P2(2)<P1(2)
-  w=-1; % clockwise
-else
-  w=+1; % counterclockwise
-end
-angle = w*atan(abs(P2(2)-P1(2))/(P2(1)-P1(1)))*180/pi; % deg
-ImgRot = imrotate(Img,angle);
+    % Rotate image
+    % clockwise OR counterclockwise
+    if eyePos2(1,2)<eyePos1(1,2)
+      w=-1; % clockwise
+    else
+      w=+1; % counterclockwise
+    end
+    angle = w*atan(abs(eyePos2(1,2)-eyePos1(1,2))/(eyePos2(1,2)-eyePos1(1,2)));%*180/pi; % deg
+    ImgRot = imrotate(mask,angle);
 
 end
 
--- tnm034.m
+++ tnm034.m
@@ -1,5 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%% 
 % Project by: Fanny Andersson och Emma Algotsson.
+% OBS!! runs only with matlab 2019!
 %
 %function id = tnm034(im) 
 % 
@@ -38,84 +39,49 @@ Eyemap = getEyeMap(Y, Cr, Cb);
 %Create mouthMap
 mouthMap = getMouthMap(image);
 
-
 %Creating a FaceMask
 FaceMask = facemask(Cr, Cb);
 
+% Combinding FaceMask and Eyemap ( get interval )
 Mask = FaceMask.*Eyemap;
-
-eyeMaxVal = max(max(Eyemap));
-mouMaxVal = max(max(mouthMap));
+% Find threshold values
+%eyeMaxVal = max(max(Eyemap));
+%mouMaxVal = max(max(mouthMap));
 
 
 %Create the binary mask
-    zeroMask = zeros(size(Eyemap));
-    faceCol = zeroMask(1,:);
-    faceRows = zeroMask(:,1);
-    mask = zeros(size(zeroMask));
+zeroMask = zeros(size(Eyemap));
+faceCol = zeroMask(1,:);
+faceRows = zeroMask(:,1);
+mask = zeros(size(zeroMask));
 
-    %Set value to 1 if ceritain interval of Cb and Cr
-    for i = 1:length(faceRows)
-        for j = 1:length(faceCol)
-            if Eyemap(i,j) > 0.45 || mouthMap(i,j) > 0.12
-                mask(i,j) = 1;
-            end   
-        end
+%Set value to 1 if ceritain interval of Cb and Cr
+for i = 1:length(faceRows)
+    for j = 1:length(faceCol)
+        if Eyemap(i,j) > 0.45 || mouthMap(i,j) > 0.12
+           mask(i,j) = 1;
+        end   
     end
-    
-    SE = strel('disk',8);
-    mask = imdilate(mask, SE);
-    mask = imdilate(mask, SE);
-    %faceMask = imdilate(faceMask, SE);
-    mask = imerode(mask, SE);
-    
-%eyeMask = mask.*YCbCr;
+end
 
-%Centroids --- 
-
-%Make mask into logical and remove border centroids
-mask = logical(mask);
+%Morphological operations to remove unnecessary blobs
+SE = strel('disk',8);
+mask = imdilate(mask, SE);
+mask = imdilate(mask, SE);
+mask = imerode(mask, SE);
 mask = imclearborder(mask);
-%Get eye candidates
-stats = regionprops('table', mask, 'centroid', 'MaxFeretProperties');
-centroids = cat(1,stats.Centroid);
-centAxis = cat(1, stats.MaxFeretDiameter);
-%Find the amount of pontential eyes
-eyesDetected = size(stats.Centroid,1);
 
-%Detect the eyes
-%First eye
-j = 1;
-for i = 1:length(eyesDetected)
-    if centAxis(i,j) > 38 && centAxis(i,j) < 60
-       firstEye = centAxis(i,j);
-      
-       firstEyePos(1,1) = centroids(i,1); 
-           
-       firstEyePos(1,2) = centroids(i,2);
-           
-    end 
-end
-%Second Eye
-j = 1;
-for i = 1:eyesDetected
-    if centAxis(i,j) > 30 && centAxis(i,j) < 60 && centAxis(i,j) ~= firstEye
-       secondEye = centAxis(i,j);
-       
-       secEyePos(1,1) = centroids(i,1); 
-            
-       secEyePos(1,2) = centroids(i,2);
-           
-       %secEyePos = reshape(secEyePos,[1,2]);
-       break
-    end   
-end
+%mask = Y.*mask;
 
-imshow(mask)
-hold on
-plot(firstEyePos)
-%plot(centroids(:,1),centroids(:,2),'b*')
-hold off
-%Im = rotateImage(mask);
+% Get Eyes and mouth position
+[eyePos1, eyePos2, mouthPos] = findTriangle(mask);
+
+ImgRot = rotateImage(eyePos1, eyePos2, mask);
+
+
+imshow(ImgRot)
+
+
+
 
 
